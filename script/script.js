79c586a903430f126053a38dd23cf895bf209c39
Sorrento21 <liujc955528@gmail.com>
2025-09-15 23:44:11 -0400

    Sep/15/2025

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector("header nav ul");
+
+    menuToggle.addEventListener("click", function() {
+        navLinks.classList.toggle("active");
+    });
+});
