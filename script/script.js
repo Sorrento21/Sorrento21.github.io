118c278dcf0d5507996d6961e5bcf0a8d6a3ec94
Sorrento21 <liujc955528@gmail.com>
2026-03-03 10:23:28 -0500

    restore files.

--- script/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector("header nav ul");
-
-    menuToggle.addEventListener("click", function() {
-        navLinks.classList.toggle("active");
-    });
-});
